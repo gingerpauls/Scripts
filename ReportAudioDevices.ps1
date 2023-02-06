1a7a1d1ed1cc91b5bddf34dc600bc00d5b1df376
gingerpauls <hutchinson9736@gmail.com>
2023-02-05 17:13:55 -0800

    more optimizations

--- ReportAudioDevices.ps1
+++ ReportAudioDevices.ps1
@@ -4,36 +4,39 @@ cls
 [console]::BufferWidth=[console]::WindowWidth
 
 #Sets random mute, and volume to all audio devices (playback and recording)
-[System.Collections.ArrayList]$playbackDevices = Get-AudioDevice -List | where {($_.Type -eq "Playback")}
-[System.Collections.ArrayList]$recordingDevices = Get-AudioDevice -List | where {($_.Type -eq "Recording")}
+[System.Collections.ArrayList]$audioDevices = Get-AudioDevice -List
 
-#open Windows Sounds
-#control mmsys.cpl sounds
 
 # save current defaults 
-$currPlay = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Playback")}
-$currPlayComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Playback")}
-$currRec = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Recording")}
-$currRecComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Recording")}
+$currPlay = $audioDevices | where {($_.Default -eq "True") -and ($_.Type -eq "Playback")}
+$currPlayComm = $audioDevices | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Playback")}
+$currRec = $audioDevices | where {($_.Default -eq "True") -and ($_.Type -eq "Recording")}
+$currRecComm = $audioDevices | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Recording")}
 
-#get playbackDevices
+#get audioDevices
 echo "Current Playback Devices"
 echo ""
 
-for ( $index = 0; $index -lt $playbackDevices.count; $index++ ) {
+for ( $index = 0; $index -lt $audioDevices.count; $index++ ) {
 	
-	Set-AudioDevice -ID $playbackDevices[$index].id | Out-Null
-	echo $playbackDevices[$index].name
-	
-	Write-Host -NoNewline "Vol: "
-	Get-AudioDevice -PlaybackVolume | Write-Host -NoNewline
-	Write-Host -NoNewline " / Mute: "
-	Get-AudioDevice -PlaybackMute | Write-Host -NoNewline
-	Write-Host -NoNewline " / ComVol: "
-	Get-AudioDevice -PlaybackCommunicationVolume | Write-Host -NoNewline
-	Write-Host -NoNewline " / ComMute: "
-	Get-AudioDevice -PlaybackCommunicationMute | Write-Host
-	
+	Set-AudioDevice -ID $audioDevices[$index].id | Out-Null
+	if($audioDevices[$index].type -eq "Playback"){
+		if($audioDevices[$index].name -eq $currPlay.name){
+			Write-Host -NoNewline "* "
+		}
+		if($audioDevices[$index].name -eq $currPlayComm.name){
+			Write-Host -NoNewline "** "
+		}
+		echo $audioDevices[$index].name
+		Write-Host -NoNewline "Vol: "
+		Get-AudioDevice -PlaybackVolume | Write-Host -NoNewline
+		Write-Host -NoNewline " / Mute: "
+		Get-AudioDevice -PlaybackMute | Write-Host -NoNewline
+		Write-Host -NoNewline " / ComVol: "
+		Get-AudioDevice -PlaybackCommunicationVolume | Write-Host -NoNewline
+		Write-Host -NoNewline " / ComMute: "
+		Get-AudioDevice -PlaybackCommunicationMute | Write-Host
+	}
 }
 
 #get recordingDevices
@@ -41,40 +44,46 @@ echo ""
 echo "Current Recording Devices:"
 echo ""
 
-for ( $index = 0; $index -lt $recordingDevices.count; $index++ ) {
-	
-	Set-AudioDevice -ID $recordingDevices[$index].id | Out-Null
-	echo $recordingDevices[$index].name
-	
-	Write-Host -NoNewline "Vol: "
-	Get-AudioDevice -RecordingVolume | Write-Host -NoNewline
-	Write-Host -NoNewline " / Mute: "
-	Get-AudioDevice -RecordingMute | Write-Host -NoNewline
-	Write-Host -NoNewline " / ComVol: "
-	Get-AudioDevice -RecordingCommunicationVolume | Write-Host -NoNewline
-	Write-Host -NoNewline " / ComMute: "
-	Get-AudioDevice -RecordingCommunicationMute | Write-Host
+for ( $index = 0; $index -lt $audioDevices.count; $index++ ) {
+	Set-AudioDevice -ID $audioDevices[$index].id | Out-Null
+	if($audioDevices[$index].type -eq "Recording"){
+		if($audioDevices[$index].name -eq $currRec.name){
+			Write-Host -NoNewline "* "
+		}
+		if($audioDevices[$index].name -eq $currRecComm.name){
+			Write-Host -NoNewline "** "
+		}
+		echo $audioDevices[$index].name
+		Write-Host -NoNewline "Vol: "
+		Get-AudioDevice -RecordingVolume | Write-Host -NoNewline
+		Write-Host -NoNewline " / Mute: "
+		Get-AudioDevice -RecordingMute | Write-Host -NoNewline
+		Write-Host -NoNewline " / ComVol: "
+		Get-AudioDevice -RecordingCommunicationVolume | Write-Host -NoNewline
+		Write-Host -NoNewline " / ComMute: "
+		Get-AudioDevice -RecordingCommunicationMute | Write-Host
+	}
+
 	
 }
 
 # restore previous audio devices
 Set-AudioDevice -ID $currPlay.id -DefaultOnly | Out-Null
 Set-AudioDevice -ID $currPlayComm.id -CommunicationOnly | Out-Null
-
 Set-AudioDevice -ID $currRec.id -DefaultOnly | Out-Null
 Set-AudioDevice -ID $currRecComm.id -CommunicationOnly| Out-Null
 
 echo ""
 # display and save current defaults 
-$currPlay = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Playback")}
-$currPlayComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Playback")}
-$currRec = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Recording")}
-$currRecComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Recording")}
+# $currPlay = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Playback")}
+# $currPlayComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Playback")}
+# $currRec = Get-AudioDevice -List | where {($_.Default -eq "True") -and ($_.Type -eq "Recording")}
+# $currRecComm = Get-AudioDevice -List | where {($_.DefaultCommunication -eq "True") -and ($_.Type -eq "Recording")}
 
 # display current defaults
-echo "Current Default Playback Device: " $currPlay.name
-echo "Current Default Playback Communication Device: " $currPlayComm.name 
-echo "Current Default Recording Device: " $currRec.name 
-echo "Current Default Recording Communication Device: " $currRecComm.name 
-echo ""
+# echo "Current Default Playback Device: " $currPlay.name
+# echo "Current Default Playback Communication Device: " $currPlayComm.name 
+# echo "Current Default Recording Device: " $currRec.name 
+# echo "Current Default Recording Communication Device: " $currRecComm.name 
+# echo ""
 Read-Host -Prompt "Press Enter to exit"
