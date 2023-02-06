c4a85636d3eff2acf2c2fc8108e85e7d99a0c876
gingerpauls <hutchinson9736@gmail.com>
2023-02-05 17:32:11 -0800

    changed to single list and removed "get-audiodevice -list"

--- RestoreGoXLRAudio.ps1
+++ RestoreGoXLRAudio.ps1
@@ -4,21 +4,19 @@
 
 echo "Fixing GoXLR Audio..."
 
-[System.Collections.ArrayList]$audioDevices = Get-AudioDevice -List | where name -like "*TC-Helicon*"
+[System.Collections.ArrayList]$tcDevices = Get-AudioDevice -List | where name -like "*TC-Helicon*"
 
 $maxVol = 100
 $unmute = 0
 
-for ( $index = 0; $index -lt $audioDevices.count; $index++ ) {
-
-	Set-AudioDevice -ID $audioDevices[$index].id | Out-Null
+for ( $index = 0; $index -lt $tcDevices.count; $index++ ) {
+	Set-AudioDevice -ID $tcDevices[$index].id | Out-Null
 	Set-AudioDevice -PlaybackVolume $maxVol
 	Set-AudioDevice -PlaybackMute $unmute
 	Set-AudioDevice -RecordingVolume $maxVol
 	Set-AudioDevice -RecordingMute $unmute
-	
 }
 
-Get-AudioDevice -List | where Type -like "Playback" | where name -like "*System*TC-Helicon*"  | Set-AudioDevice -Verbose -DefaultOnly | Out-Null
-Get-AudioDevice -List | where Type -like "Playback" | where name -like "*Chat*TC-Helicon*"  | Set-AudioDevice -Verbose -CommunicationOnly | Out-Null
-Get-AudioDevice -List | where Type -like "Recording" | where name -like "*Mic*TC-Helicon*"  | Set-AudioDevice -Verbose | Out-Null
+$tcDevices | where Type -like "Playback" | where name -like "*System*TC-Helicon*"  | Set-AudioDevice -Verbose -DefaultOnly #| Out-Null
+$tcDevices | where Type -like "Playback" | where name -like "*Chat*TC-Helicon*"  | Set-AudioDevice -Verbose -CommunicationOnly #| Out-Null
+$tcDevices | where Type -like "Recording" | where name -like "*Mic*TC-Helicon*"  | Set-AudioDevice -Verbose #| Out-Null
