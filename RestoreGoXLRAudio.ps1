b4c66743a1fe07e5ca03cc73ca5015c2b9bddcb0
gingerpauls <hutchinson9736@gmail.com>
2023-02-05 17:19:21 -0800

    changed to single

--- RestoreGoXLRAudio.ps1
+++ RestoreGoXLRAudio.ps1
@@ -1,27 +1,22 @@
 [console]::WindowWidth=30; 
 [console]::WindowHeight=2; 
 [console]::BufferWidth=[console]::WindowWidth
+
 echo "Fixing GoXLR Audio..."
-[System.Collections.ArrayList]$tcPlaybackDevices = Get-AudioDevice -List | where {($_.Type -eq "Playback")} | where name -like "*TC-Helicon*"
-[System.Collections.ArrayList]$tcRecordingDevices = Get-AudioDevice -List | where {($_.Type -eq "Recording")} | where name -like "*TC-Helicon*"
+
+[System.Collections.ArrayList]$audioDevices = Get-AudioDevice -List | where name -like "*TC-Helicon*"
 
 $maxVol = 100
 $unmute = 0
 
-for ( $index = 0; $index -lt $tcPlaybackDevices.count; $index++ ) {
+for ( $index = 0; $index -lt $audioDevices.count; $index++ ) {
 
-	Set-AudioDevice -ID $tcPlaybackDevices[$index].id | Out-Null
+	Set-AudioDevice -ID $audioDevices[$index].id | Out-Null
 	Set-AudioDevice -PlaybackVolume $maxVol
 	Set-AudioDevice -PlaybackMute $unmute
-	
-}
-
-for ( $index = 0; $index -lt $tcRecordingDevices.count; $index++ ) {
-
-	Set-AudioDevice -ID $tcRecordingDevices[$index].id  | Out-Null
 	Set-AudioDevice -RecordingVolume $maxVol
 	Set-AudioDevice -RecordingMute $unmute
-
+	
 }
 
 Get-AudioDevice -List | where Type -like "Playback" | where name -like "*System*TC-Helicon*"  | Set-AudioDevice -Verbose -DefaultOnly | Out-Null
