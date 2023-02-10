e439956cb33919c64c14b82a8a866954b4aa2fe5
Jordan <j@j.com>
2023-02-10 16:17:50 -0600

    Added some output for RunTest.ps1 to say what's happening

--- RunTest.ps1
+++ RunTest.ps1
@@ -3,8 +3,8 @@ Function RunAndTime {
     Param(
         [string]$script
     )
-        [console]::WindowWidth=80; 
-        [console]::WindowHeight=86; 
+        #[console]::WindowWidth=80; 
+        #[console]::WindowHeight=86; 
         [console]::BufferWidth=[console]::WindowWidth
         $StopWatch = [system.diagnostics.stopwatch]::startNew()
         
@@ -16,16 +16,20 @@ Function RunAndTime {
     
     #RunAndTime "D:\Scripts\WrongAudio.ps1"
     ## randomize all audio
+    Write-Host "Randomizing..."
     RunAndTime "D:\Scripts\CAudioDevices\build\audio.exe -r"
 
     #RunAndTime "D:\Scripts\RestoreGoXLRAudio.ps1"
     ## unmute, max volume, and set TC-Helicon devices to default
+    Write-Host "Setting default devices..."
     RunAndTime "D:\Scripts\CAudioDevices\build\audio.exe -TC"
 
     #RunAndTime "D:\Scripts\RestoreAllAudio.ps1"
     ## unmute, max volume all devices
+    Write-Host "Unmuting and maxing all devices..."
     RunAndTime "D:\Scripts\CAudioDevices\build\audio.exe -u *"
     
     #RunAndTime "D:\Scripts\ReportAudioDevices.ps1"
     ## list all devices
+    Write-Host "Listing all devices..."
     RunAndTime "D:\Scripts\CAudioDevices\build\audio.exe -l"
